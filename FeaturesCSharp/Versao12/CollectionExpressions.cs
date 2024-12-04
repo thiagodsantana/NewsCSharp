771b54666a77110f8cdeeb91f63aee3d5578c877
Thiago Santana <thiago.darley@gmail.com>
2024-12-04 13:53:57 -0300

    Versão C# 12

--- FeaturesCSharp/Versao12/CollectionExpressions.cs
+++ FeaturesCSharp/Versao12/CollectionExpressions.cs
@@ -2,11 +2,39 @@
 {
     internal class CollectionExpressions
     {
+        /*
+         * Resumo:
+            * Benefícios:
+                * Legibilidade: 
+                    *   Código mais limpo
+                * Redução de Boilerplate:
+                    *   Menos necessidade de código adicional para inicialização de coleções.
+                * Unificação de Sintaxe: 
+                    *   Facilita a transição entre diferentes tipos de coleções
+            * Limitações:
+                    *   Compatibilidade: O tipo deve implementar ICollection<T> ou IReadOnlyCollection<T>
+                    *   Limitações de Sintaxe: Ainda em preview
+         */
         public CollectionExpressions()
         {
-            ////List<int> numeros = [1, 2, 3, 4, 5];
-            ////HashSet<string> palavras = ["maçã", "banana", "laranja"];
-            ////Dictionary<int, string> mapa = [1 => "um", 2 => "dois", 3 => "três"];
+            // Criação de listas
+            var listaCE = [10, 20, 30];
+            Console.WriteLine(string.Join(", ", listaCE)); // Saída: 10, 20, 30
+
+            // Intepretação do compilador
+            var lista = new List<int> { 10, 20, 30 };
+
+
+            // Criação de dicionários
+            var dicionarioCE = ["um" => 1, "dois" => 2];
+            Console.WriteLine(dicionarioCE["um"]); // Saída: 1
+
+            // Interpretação do compilador
+            var dicionario = new Dictionary<string, int>
+            {
+                { "um", 1 },
+                { "dois", 2 }
+            };
         }
     }
 }
--- FeaturesCSharp/Versao12/InlineArrays.cs
+++ FeaturesCSharp/Versao12/InlineArrays.cs
@@ -2,10 +2,28 @@
 {
     internal class InlineArrays
     {
+        /*
+         * Resumo:
+            * Benefícios:
+                *  Concorrência e Legibilidade: A sintaxe simplificada permite uma inicialização rápida e direta de arrays em qualquer lugar.
+                *  Inferência de Tipo: O compilador infere automaticamente o tipo do array, o que economiza digitação e torna o código mais limpo.
+                *  Melhor Integração com Expressões e Métodos: Ao criar arrays diretamente dentro de expressões ou chamadas de métodos, o código se torna mais conciso e flexível.
+                * 
+         */
         public InlineArrays()
         {
-            var numeros = new[] { 1, 2, 3 };
-            Console.WriteLine(numeros);
+            // Definindo um array de forma simplificada (sem precisar de 'new')
+            var numeros = [1, 2, 3, 4];
+
+            // Como o compilador interpreta:
+            var numeros2 = new int[] { 1, 2, 3, 4 };
+
+            // Segurança nos tipos:
+            var misturado = [1, "texto", 3.5];  // Erro de compilação
+
+
+            Console.WriteLine(string.Join(", ", numeros));  // Saída: 1, 2, 3, 4
+
         }
     }
 }
